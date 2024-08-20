aba3bfffe77a2e0a68cb829f0a8727b624a3902b
Felipe Almeida <92704880+Splef13@users.noreply.github.com>
2024-08-20 23:31:09 +0000

    Código da aula 20/08/2024

---
+++ aula04/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program{
+    public static void Main (string [] args){
+        Pessoa objPessoa = new Pessoa("Rafael");
+        Console.WriteLine("Antes do método (Classe): " + objPessoa.ToString());
+        AlterarNomeClasse(objPessoa);
+        Console.WriteLine("Depois do método (Classe): " + objPessoa.ToString());
+
+        PessoaStruct pessoaStruct = new PessoaStruct("Miguel");
+        Console.WriteLine("Antes do método (struct): " + pessoaStruct.ToString());
+        AlteraNomeStruct(pessoaStruct);
+        Console.WriteLine("Depois do método (struct): " + pessoaStruct.ToString());
+    }
+
+    public static void AlterarNomeClasse(Pessoa objAux){
+        objAux.Nome = "Almeida";
+    }
+
+    public static void AlteraNomeStruct(PessoaStruct objAux){
+        objAux.NomeStruct = "Talita";
+    }
+}
